5c15ec99c39020701753d190f30f55ae65c56116
Jawann <jawann@gmail.com>
2017-12-10 14:28:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,11 @@
 class Triangle
-  def kind (size_a, size_b, size_c)
-    return :equilateral if size_a == size_b && size_b == size_c
+  attr_accessor :size_a, :size_b, :size_c
+
+  def initialize (a, b, c)
+    @size_a, @size_b, @size_c = a, b, c
+  end
+
+  def kind
+    return :equilateral if @size_a == @size_b && @size_b == @size_c
   end
 end
